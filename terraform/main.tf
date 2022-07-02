eca5480f88a5793ebbd1fd52009e16f92edfa509
Jason Lehmer <jason@lehmer.org>
2022-07-02 09:44:02 -0500

    Add dynamodb table to lock tfstate

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,28 +7,14 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "expandedelements-terraform-state"
-    key    = "tts-article-reader"
-    region = "us-east-2"
+    bucket         = "expandedelements-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+    key            = "tts-article-reader"
+    region         = "us-east-2"
   }
 }
 
 provider "aws" {
   region = "us-east-2"
-}
-
-resource "aws_s3_bucket" "tfstate" {
-  bucket = var.tfstate_bucketname
-}
-
-resource "aws_s3_bucket_acl" "tfstate" {
-  bucket = aws_s3_bucket.tfstate.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_versioning" "tfstate" {
-  bucket = aws_s3_bucket.tfstate.id
-  versioning_configuration {
-    status = "Enabled"
-  }
 }
---
+++ terraform/s3.tf
@@ -0,0 +1,15 @@
+resource "aws_s3_bucket" "tfstate" {
+  bucket = var.tfstate_bucketname
+}
+
+resource "aws_s3_bucket_acl" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
