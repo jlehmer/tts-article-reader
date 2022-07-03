3e03a4fa4961f66423a06dae4b1fe48d76800639
Jason Lehmer <jason@lehmer.org>
2022-07-03 00:07:18 -0500

    Fix failing tests

--- test/handler/ArticleReader.test.ts
+++ test/handler/ArticleReader.test.ts
@@ -1,9 +1,28 @@
 // eslint-disable-next-line import/no-unresolved
 import { Context, SNSEvent } from 'aws-lambda';
 import * as handler from '../../src/handler/ArticleReader';
+import { ArticleExtractService } from '../../src/service/ArticleExtractService';
 
 describe('Article Reader handler tests', () => {
-  it('Mock test', async () => {
-    await handler.articleReader({} as SNSEvent, {} as Context, null);
+  const mockRetrieveArticle = jest.fn();
+  const mockEvent = {
+    Records: [
+      {
+        Sns: {
+          Message: 'mockUrl'
+        }
+      }
+    ]
+  };
+
+  beforeEach(() => {
+    mockRetrieveArticle.mockReset();
+  });
+
+  it('articleReader() does not throw an error when a successful extraction is done', async () => {
+    ArticleExtractService.prototype.retrieveArticle = mockRetrieveArticle;
+    mockRetrieveArticle.mockResolvedValueOnce({ text: 'mock article text' });
+
+    await handler.articleReader(mockEvent as SNSEvent, {} as Context, null);
   });
 });
