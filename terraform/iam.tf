24265b37e3e73df5a090d2e0042dc27d85062e41
Jason Lehmer <jason@lehmer.org>
2022-07-03 09:13:37 -0500

    Fix IAM formatting

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -4,14 +4,14 @@ resource "aws_iam_policy" "lambda_iam_policy" {
   policy = <<POLICY
 {
   "Version": "2012-10-17",
-  "Statement": {
+  "Statement": [{
       "Sid": "APIAccessForDynamoDBStreams",
       "Effect": "Allow",
       "Action": [
           "dynamodb:PutItem"
       ],
       "Resource": ["${aws_dynamodb_table.article_table.arn}"]
-  }
+  }]
 }
 POLICY
 }
