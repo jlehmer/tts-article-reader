25c64e55900d96f87656d01099db699274ff054b
Jason Lehmer <jason@lehmer.org>
2022-07-04 00:05:00 -0500

    Remove raw message requirement

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -3,8 +3,7 @@ resource "aws_sns_topic" "tts_results" {
 }
 
 resource "aws_sns_topic_subscription" "tts_result_lambda_subscription" {
-  topic_arn            = aws_sns_topic.tts_results.arn
-  protocol             = "lambda"
-  endpoint             = module.tts_result_lambda.lambda_function_arn
-  raw_message_delivery = true
+  topic_arn = aws_sns_topic.tts_results.arn
+  protocol  = "lambda"
+  endpoint  = module.tts_result_lambda.lambda_function_arn
 }
