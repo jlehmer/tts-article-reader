8bb607d55e96fdcc7b8c090f9bcf597e271c8bdd
Jason Lehmer <jason@lehmer.org>
2022-07-03 23:57:06 -0500

    Create tts-result topic and subscription

---
+++ terraform/sns.tf
@@ -0,0 +1,10 @@
+resource "aws_sns_topic" "tts_results" {
+  name = "tts-results"
+}
+
+resource "aws_sns_topic_subscription" "tts_result_lambda_subscription" {
+  topic_arn            = aws_sns_topic.tts_results.arn
+  protocol             = "lambda"
+  endpoint             = module.tts_result_lambda.lambda_function_arn
+  raw_message_delivery = true
+}
