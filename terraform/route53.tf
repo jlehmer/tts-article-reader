c85da80997d21634a4a9925c593af579793b9514
Jason Lehmer <jason@lehmer.org>
2022-07-13 21:50:58 -0500

    Change up route53 record syntax

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -2,10 +2,19 @@ resource "aws_route53_zone" "main" {
   name = var.domain_name
 }
 
-resource "aws_route53_record" "validation_record" {
-  zone_id = aws_route53_zone.main.zone_id
-  name    = aws_acm_certificate.article_reader_domain.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.article_reader_domain.domain_validation_options.0.resource_record_type
-  records = [aws_acm_certificate.article_reader_domain.domain_validation_options.0.resource_record_value]
-  ttl     = 300
+resource "aws_route53_record" "article_reader" {
+  for_each = {
+    for dvo in aws_acm_certificate.article_reader_domain.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.main.zone_id
 }
