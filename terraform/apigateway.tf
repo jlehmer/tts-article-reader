803958cb12181e020a603a43f9f6d82fa262ab96
Jason Lehmer <jason@lehmer.org>
2022-07-23 08:42:44 -0500

    Fix wrong http method for todoist endpoint

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -70,7 +70,7 @@ resource "aws_api_gateway_resource" "todoist" {
 resource "aws_api_gateway_method" "todoist_post" {
   rest_api_id   = aws_api_gateway_rest_api.article_reader.id
   resource_id   = aws_api_gateway_resource.todoist.id
-  http_method   = "GET"
+  http_method   = "POST"
   authorization = "NONE"
 }
 
