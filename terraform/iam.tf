44ab093674e12bd14958778e239237c90a5f336c
Jason Lehmer <jason@lehmer.org>
2022-07-22 20:27:52 -0500

    Rework sns log role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -74,6 +74,20 @@ resource "aws_iam_role" "sns_log_role" {
     Version = "2012-10-17"
     Statement = [
       {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "sns.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
         Action = [
           "logs:CreateLogStream",
           "logs:DescribeLogStreams",
