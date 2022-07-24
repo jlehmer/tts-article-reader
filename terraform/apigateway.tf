969c862d046c8bec444222af762f0bbd2d7262c9
Jason Lehmer <jason@lehmer.org>
2022-07-23 20:33:19 -0500

    Reference stage name differently in method settings block

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -62,7 +62,7 @@ resource "aws_api_gateway_stage" "prod" {
 
 resource "aws_api_gateway_method_settings" "general_settings" {
   rest_api_id = aws_api_gateway_rest_api.article_reader.id
-  stage_name  = "prod"
+  stage_name  = aws_api_gateway_stage.prod.stage_name
   method_path = "*/*"
 
   settings {
